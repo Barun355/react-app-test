5f2dfac9c1118dd09accebfbc8d2672f64326081
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-02-28 19:50:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import Button from './components/Button';
 
 function App(){
   return(
     <div>
       <Header name="Shreyas" />
-      <Main greet="Hello" />
-      <Button value="View Profile" />
+      <Main />
     </div>
   );
 }
