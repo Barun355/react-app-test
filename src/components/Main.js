43cae600891859ec4bbec134768e53045e94f160
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-02-28 17:41:14 +0530

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import ishushreyas from './ishushreyas.jpg';
 
 function Main(props){
   return (
-    <div className={main}>
+    <div className="main">
       <img src={ishushreyas}>
     </div>
   );
