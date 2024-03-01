f2a5aa42cbdc81dcdca02d61d66bbf04fefa882f
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-01 19:08:20 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+function Footer(props){
+  return(
+    <div className="footer">ishushreyas<span><span className="material-symbols-rounded">feedback</span></span></div>
+  );
+}
+
+export default Footer;
---
+++ Main.js
@@ -0,0 +1,17 @@
+import Button from './Button';
+import TextView from './TextView';
+
+import ishushreyas from './ishushreyas.jpg';
+import './Main.css';
+
+function Main(props){
+  return (
+    <div className="main">
+      <img src={ishushreyas} alt="ishushreyas" className="main-img"/>
+      <TextView>Ishu Shreyas</TextView>
+      <Button>View Profile</Button>
+    </div>
+  );
+}
+
+export default Main;
