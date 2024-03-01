585956fd7b7816524d4d404aea8abccf67768549
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-01 19:09:35 +0530

    Delete Footer.js

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-
-function Footer(props){
-  return(
-    <div className="footer">ishushreyas<span><span className="material-symbols-rounded">feedback</span></span></div>
-  );
-}
-
-export default Footer;
