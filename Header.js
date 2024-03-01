a19eb312cc96972efd11202a0263d1a43bd6a128
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-01 19:10:35 +0530

    Delete Header.js

--- Header.js
+++
@@ -1,9 +0,0 @@
-import './Header.css';
-
-function Header(props){
-  return(
-    <div className="header"><h2>{props.name}</h2><span><span className="material-symbols-rounded">account_circle</span></span></div>
-  );
-}
-
-export default Header;
