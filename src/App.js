b910ea8c1dfb752479cf3d444734aadd27f46a0a
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-01 20:49:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import Footr from './components/Footer';
 
 function App(){
   return(
     <div>
       <Header name="Shreyas" />
       <Main />
+      <Footer />
     </div>
   );
 }
