91d11909445f33d40c2811dcf69e77f44495317f
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-03 21:45:44 +0530

    Add files via upload

--- src/components/Button.js
+++ src/components/Button.js
@@ -38,7 +38,7 @@ const Button = ({children}) => {
   };
 
   return (
-    <button className="button-class" onClick={ripple}>
+    <button className="button-class" onTouchStart={ripple}>
       {children}
     </button>
   );
