6467e51b839257ec89b9eec4911eef084af5f021
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-03 21:35:56 +0530

    Add files via upload

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,22 @@
 import './Button.css';
+import React, { useState, useEffect } from 'react';
 
 const Button = ({children}) => {
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollValue = window.scrollY || document.documentElement.scrollTop;
+
+      setScrollValue(scrollValue);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const ripple = (event) => {
     const button = event.currentTarget;
@@ -12,7 +28,7 @@ const Button = ({children}) => {
 
     circle.style.width = circle.style.height = `${diameter}px`;
     circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.style.top = `${scrollValue + event.clientY - button.offsetTop - radius}px`;
 
     button.appendChild(circle);
 
