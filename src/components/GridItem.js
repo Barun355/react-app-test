52cf013c95ca83146dd56c811e2f128d2782f7b8
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-12 20:54:45 +0530

    Add files via upload

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,8 +1,50 @@
 import './GridItem.css';
 
 function GridItem({elevated, children}){
+  const [scrollXValue, setScrollXValue] = useState(0);
+  const [scrollYValue, setScrollYValue] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollYValue = window.scrollY || document.documentElement.scrollTop;
+
+      setScrollYValue(scrollYValue);
+      const scrollXValue = window.scrollX || document.documentElement.scrollLeft;
+
+      setScrollXValue(scrollXValue);
+      
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const ripple = (event) => {
+    const buttonClickAction = clickAction || console.log("Button clicked");
+    const button = event.currentTarget;
+    const circle = document.createElement('span');
+    circle.classList.add('ripple');
+
+    const diameter = Math.max(button.offsetWidth, button.offsetHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${scrollXValue + event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${scrollYValue + event.clientY - button.offsetTop - radius}px`;
+
+    button.appendChild(circle);
+
+    setTimeout(() => {
+      button.removeChild(circle);
+      buttonClickAction();
+    }, 400);
+  };
+  
   return (
-    <div className={`grid-item ${ elevated ? "elevated" : ""}`}>
+    <div className={`grid-item ${ elevated ? "elevated" : ""}`} onClick={ripple}>
       {children}
     </div>
   );
