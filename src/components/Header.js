d88156b307d6f968c110350c0c79841b225e5ba7
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-03-16 15:23:10 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import './Header.css';
 import Button from './Button';
+import React, { useState, useEffect } from 'react';
 
 function Header(props){
+  const [scrollYValue, setScrollYValue] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollYValue = window.scrollY || document.documentElement.scrollTop;
+      setScrollYValue(scrollYValue);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    
+  }, []);
+
   const handleClick = () => {
     window.open("mailto:ishushreyas@gmail.com");
   };
   
   return(
-    <div className="header"><p>{props.name}</p><span><Button clickAction={handleClick} positionFixed>Get in touch</Button></span></div>
+    (scrollYValueY > 150) ? <div className="header-open"><p>{props.name}</p><span><Button clickAction={handleClick} positionFixed>Get in touch</Button></span></div> : <div className="header"><p>{props.name}</p><span><Button clickAction={handleClick} positionFixed>Get in touch</Button></span></div>;
   );
 }
 
