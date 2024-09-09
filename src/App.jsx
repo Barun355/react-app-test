d8f6e1991156adc0130bcbd2c164728a32bb2654
BARUN TIWARY <67851907+Barun355@users.noreply.github.com>
2024-09-09 20:29:17 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className='flex justify-center items-center h-screen w-full' style={{ backgroundColor: "#333", color: "white"}}>
-      <h1 style={{fontFamily: "Inter"}}>BnS comming soon...</h1>
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-full'>
+      <figure>
+        <span className='bg-clip-text bg-gradient-to-tr from-blue-500 text-2xl font-bold'>BnS Tech</span>
+      </figure>
+    </div>
+  )
+}
+
+export default Header
